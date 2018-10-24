c034ef02aee38587495a48f0f3b26743628fae68
Vincent Beauvieux <vbeauvieux@gmail.com>
2018-10-24 18:36:04 +0200

    add top margin to all screen if ios

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import ConverterScreenContainer from "./src/converter/ConverterScreenContainer";
 import { Provider } from "react-redux";
 import store from "./src/redux/Store";
 
 export default class App extends React.Component<{}> {
   render() {
+    let statusBarHeight = { marginTop: 0 };
+    if (Platform.OS === "ios") {
+      statusBarHeight.marginTop = 20;
+    }
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, statusBarHeight]}>
         <Provider store={store}>
           <ConverterScreenContainer />
         </Provider>
